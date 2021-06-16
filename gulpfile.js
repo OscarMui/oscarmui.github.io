94b7287666c8810132bc7b779f2e0d5fd98226c9
Oscar Mui <oscarmui1127@gmail.com>
2021-06-16 22:51:08 +0800

    prepare template for grad stuff

--- gulpfile.js
+++ gulpfile.js
@@ -21,53 +21,53 @@ gulp.task('default', function(done){
 //pug to html conversion
 gulp.task('pug', function(){
   return gulp.src("app/templates/views/*.pug")
-  .pipe(changed("dist/")) //pipe files only if changed 
+  .pipe(changed("docs/")) //pipe files only if changed 
   .pipe(pug({pretty:true})) //pug to html
-  .pipe(gulp.dest('dist/'));
+  .pipe(gulp.dest('docs/'));
 });
 
 //less to css conversion
 gulp.task('less', function(){
   return gulp.src("app/styles/site.less")
-  .pipe(changed("dist/styles")) //pipe files only if changed 
+  .pipe(changed("docs/styles")) //pipe files only if changed 
   .pipe(less({
     plugins: [autoprefix]
   })) //less to css
-  .pipe(gulp.dest('dist/styles'));
+  .pipe(gulp.dest('docs/styles'));
 });
 
 //optimise js
 gulp.task('js',function(){
   return gulp.src("app/js/**/*.js")
-  //.pipe(changed("dist/js"))//pipe files only if changed 
+  //.pipe(changed("docs/js"))//pipe files only if changed 
   .pipe(eslint({
     configFile: '.eslintrc'
   }))//eslint: check if js has any errors
   // .pipe(babel({
   //   presets: ['@babel/env']
   // })) //babel: change js to traditional js syntax
-  .pipe(gulp.dest("dist/js"));  
+  .pipe(gulp.dest("docs/js"));  
 });
 
 //minify image
 gulp.task('imagemin',function(){
   return gulp.src("app/images/**/*")
-  .pipe(changed("dist/images"))//pipe files only if changed 
+  .pipe(changed("docs/images"))//pipe files only if changed 
   .pipe(imagemin())
-  .pipe(gulp.dest("dist/images"));
+  .pipe(gulp.dest("docs/images"));
 });
 
 gulp.task('imagecopy',function(){
   return gulp.src("app/images/**/*")
-  .pipe(changed("dist/images"))//pipe files only if changed 
-  .pipe(gulp.dest("dist/images"));
+  .pipe(changed("docs/images"))//pipe files only if changed 
+  .pipe(gulp.dest("docs/images"));
 });
 
 
 gulp.task('fontcopy',function(){
   return gulp.src("app/fonts/*")
-  .pipe(changed("dist/images"))//pipe files only if changed 
-  .pipe(gulp.dest("dist/fonts"));
+  .pipe(changed("docs/images"))//pipe files only if changed 
+  .pipe(gulp.dest("docs/fonts"));
 });
 
 
